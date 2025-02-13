84339621e7e2eb38acd6b4a03216adf7b909d67a
Kosui Iwasa <24590869+iwasa-kosui@users.noreply.github.com>
2025-02-13 17:27:57 +0900

    refactor: AirconApplianceを返す

--- src/actions/getAirconAppliance.ts
+++ src/actions/getAirconAppliance.ts
@@ -3,8 +3,9 @@
 import { useCache } from '@lazarv/react-server/memory-cache'
 import { RemoClient } from '../client/remo'
 import { Env } from '../env'
+import { AirconAppliance } from '../domain/appliance'
 
-export async function getAirconAppliance() {
+export async function getAirconAppliance(): Promise<AirconAppliance> {
   const client = RemoClient.from(Env.API_KEY)
   return await useCache(
     ['AirconAppliance'],
